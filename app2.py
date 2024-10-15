26b1aa4ca542e5ed2f02bc3b74a4fb50a5b43c52
UmangPatel3010 <96334587+UmangPatel3010@users.noreply.github.com>
2024-10-15 15:32:06 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import pickle
+from skimage.transform import resize
+# from tensorflow.keras.models import load_model
+
+
+# Define categories
+categories = ['Organic', 'Recycle']
+
+# Load the trained model
+# model = load_model("model.p")
+# model.summary()
+with open("model.p", 'rb') as f:
+    model = pickle.load(f)
+
+# Streamlit UI
+st.set_page_config(page_title="Waste Classification",layout="wide", page_icon="./favicon.png")
+st.write('<style>div.block-container{padding-top:2rem;}</style>', unsafe_allow_html=True)
+st.title(":green[Waste Classification App]")
+
+# Create two columns: one for the image, one for the button and result
+
+uploaded_file = st.file_uploader("Upload the waste Image", type=["jpg", "jpeg", "png"])
+col1, col2, _ = st.columns(3, gap="large")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    col1.image(image, caption="Uploaded Image", use_column_width="auto")
+
+    if col2.button("Classify"):
+        image = np.array(image)
+        image = resize(image, (50, 50, 3))  # Resize image to match model input
+        image = np.expand_dims(image, axis=0)  # Add batch dimension
+
+        prediction = model.predict(image)
+
+        label = list(prediction[0]).index(max(prediction[0]))
+
+        s = f"<p style='font-size:30px;'>Prediction: <b style='color:{"#64f562" if label == 0 else "#706bff"}';>{categories[label]} waste</b></p>"
+        col2.markdown(s, unsafe_allow_html=True)
